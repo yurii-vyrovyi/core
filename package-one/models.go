a69b5c202b8fceafa054fc20e684e50d2ca10418
Yurii Vyrovyi <yvyrovyi@external.cinemo.com>
2023-11-27 12:41:08 +0100

    commit #4 package-one v0.2.0

--- package-one/models.go
+++ package-one/models.go
@@ -1,32 +1,6 @@
 package package_one
 
-import (
-	"math/rand"
-)
+const Version = "v0.2.0"
 
-const Version = "v0.0"
-
-
-type DataStructTypeOne struct {
-	ID      string `json:"id"`
-	Message string `json:"message"`
-}
-
-func GenerateRandomData() DataStructTypeOne {
-	return DataStructTypeOne{
-		ID:      randSeq(8),
-		Message: randSeq(12),
-	}
-}
-
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
 
 
